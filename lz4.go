2d6951c073e97abbbeb59c0702de6fe92e40fea9
id01 <gaviniboom@gmail.com>
2019-01-19 16:53:17 -0800

    new file:   .travis.yml 	new file:   lz4.go

---
+++ lz4.go
@@ -0,0 +1,22 @@
+package lz4
+
+/*
+#cgo LDFLAGS: -llz4
+#cgo CFLAGS: -O3
+#include "lz4frame.h"
+*/
+import "C"
+
+import (
+	"unsafe"
+	"errors"
+)
+
+func LZ4_compressFrame(in []byte) (out []byte, err error) {
+	out = make([]byte, (len(in)<<1)+128)
+	n := C.LZ4F_compressFrame(unsafe.Pointer(&in), C.ulong(len(in)), unsafe.Pointer(&out), C.ulong(len(out)), nil)
+	if n <= 0 {
+		return out, errors.New("Compression error")
+	}
+	return out, nil
+}
