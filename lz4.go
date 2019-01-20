d36aa49c224888f5e65e6a47c78e48c7547b73d1
id01 <gaviniboom@gmail.com>
2019-01-19 17:33:00 -0800

    modified:   lz4.go

--- lz4.go
+++ lz4.go
@@ -12,11 +12,11 @@ import (
 	"errors"
 )
 
-func LZ4_compressFrame(in []byte) (outArr []byte, err error) {
-	out := make([]byte, (len(in)<<1)+128)
-	n := C.LZ4F_compressFrame(unsafe.Pointer(&in[0]), C.ulong(len(in)), unsafe.Pointer(out), C.ulong(len(out)), nil)
-	if n <= 0 {
+func LZ4_compressFrame(in []byte) (out []byte, err error) {
+	out = make([]byte, (len(in)<<1)+128) // Overestimate
+	n := C.LZ4F_compressFrame(unsafe.Pointer(&out[0]), C.ulong(len(out)), unsafe.Pointer(&in[0]), C.ulong(len(in)), nil)
+	if n <= 0 || n >= 9223372036854775807 {
 		return out, errors.New("Compression error")
 	}
-	return out, nil
+	return out[:n], nil
 }
